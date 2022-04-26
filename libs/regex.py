1d39c73fb36e557954beb0f5d292daa85b8ecda8
li_jinmiao <li_jinmiao@venusgroup.com.cn>
2022-04-26 22:46:04 +0800

    [libs] add 'img/video/executable/common domain' regex

--- libs/regex.py
+++ libs/regex.py
@@ -15,17 +15,19 @@ http_url_find_regex = re.compile(r'http[s]?://[\w\-.:]+\w+[\w./?&=+#%-]+')
 nohttp_url_find_regex = re.compile(r'(?:[\w\-]{1,128}\.){1,16}\w+(?:\:\d+)?[/?][\w./?&=+#%-]+')
 
 
-# html = re.compile(r".*\.(html|htm|shtml|shtm|xhtml|asp|jsp|php)$", re.I)
+html = re.compile(r".*\.(html|htm|html5|shtml|shtm|xhtml|asp|aspx|jsp|jspx|php|do)$", re.I)
+img = re.compile(r".*\.(jp|jpg|jpeg|jpgv|gif|png|pngc|ico|bmp|svg|pic|tif|tiff|psd)$", re.I)
+video = re.compile(r".*\.(mp4|mp3|avi|mkv|flv|3gp|ts|m3u8|wav|mov|wmv|wmx)$", re.I)
+executable = re.compile(r".*\.(exe|apk|msi|iso|bin|jar|class|rpm|deb|whl|dbf)$", re.I)
 file = re.compile(r".*\.("
                   r"jpg|jpeg|gif|png|ico|bmp|svg|pic|tif|tiff|psd|xcf|cdr|eps|indd|"
-                  r"csv|pdf|doc|docx|xls|xlsx|xltx|ppt|pptx|vsd|vsdx|chm|odt|swf|xml|"
+                  r"txt|csv|pdf|doc|docx|xls|xlsx|xltx|ppt|pptx|vsd|vsdx|chm|odt|swf|xml|"
                   r"zip|tar|7z|rar|gz|tgz|xz|bz|bz2|exe|apk|msi|iso|bin|"
                   r"mp4|mp3|avi|mkv|flv|3gp|ts|m3u8|wav|mov|wmv|wmx|"
                   r"jar|class|rpm|deb|whl|dbf"
                   r")$", re.I)
 gov_edu = re.compile(r"^([0-9a-zA-Z][0-9a-zA-Z\-]{0,62}\.){1,255}(gov|edu)(\.[a-zA-Z]{2})?")
-
-common_suffix = {'com', 'cn', ''}
+common_dom = re.compile(r".*\.(com|cn|net|org|info|edu|gov|mil|top|biz|int|name|tw|(gov|edu)\.[a-zA-Z]{2})$", re.I)
 
 
 def is_valid_ip(text):
